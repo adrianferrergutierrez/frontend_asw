16eafc4f7776fbdfaa117bce858564bfaf278e87
adrianferrergutierrez <adrian.ferrer.gutierrez@estudiantat.upc.edu>
2025-05-29 22:39:57 +0200

    cambio de apikey

--- mi_frontend/src/services/api.ts
+++ mi_frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'https://waslab04-p1hk.onrender.com/api/v1';
-const API_KEY = 'OPGPjVjDxM7u5jshpus4tKiTFjfplKtA';
+const API_KEY = 'FPNzyupb0yhy7tA3Ey8UT0fSvi4rbIrf';
 
 const api = axios.create({
     baseURL: API_URL,
